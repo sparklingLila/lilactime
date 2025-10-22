fc64dfbb6b63c5f8ba24ccb3352fb617ee5b3e9a
sparklingLila <ichigonokokoro@gmail.com>
2025-10-22 20:41:13 -0300

    Add key attr to stamp list elements

--- src/components/HomeElements/Stamps.jsx
+++ src/components/HomeElements/Stamps.jsx
@@ -7,7 +7,7 @@ export const Stamps = () => {
       <h1 className="text-pink2"><img src="img/sparkles.gif" alt="sparkles gif" /> STAMPS</h1>
       <div className='stamp-container'>
         {stamps.map(item => (
-          <a href={item.source} target="_blank"><img src={item.url} alt={item.alt} className='stamp' /></a>
+          <a href={item.source} target="_blank" key={item.id}><img src={item.url} alt={item.alt} className='stamp' /></a>
         ))}
       </div>
     </>
